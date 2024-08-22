aa58828c8a5dd454502e595364d59fb57c549377
jwc5388 <135943909+jwc5388@users.noreply.github.com>
2024-08-22 17:31:27 +0900

    Updates

---
+++ precache-manifest.49bcb5783e056f8a735b614bb8437fef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83ae0728580ac071dfd24bb87946d13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37c3c933d54f1ad4aafc",
+    "url": "/static/css/main.da495b6e.chunk.css"
+  },
+  {
+    "revision": "b85007b0cf9e5329b606",
+    "url": "/static/js/2.5afa37b6.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.5afa37b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e408eec957227aaebca",
+    "url": "/static/js/3.16f80f2e.chunk.js"
+  },
+  {
+    "revision": "37c3c933d54f1ad4aafc",
+    "url": "/static/js/main.b0b6f1b4.chunk.js"
+  },
+  {
+    "revision": "fbe85701f69f22ac26ae",
+    "url": "/static/js/runtime-main.cb7aea12.js"
+  },
+  {
+    "revision": "5bbfe2abff33dcdce7a45eefd33daf57",
+    "url": "/static/media/CSDissertation.5bbfe2ab.pdf"
+  },
+  {
+    "revision": "2b362807d032562105be7c66754f385d",
+    "url": "/static/media/MathDissertation.2b362807.pdf"
+  },
+  {
+    "revision": "fc5fff98ee8c606ab170a1b672a2e144",
+    "url": "/static/media/cotons1.fc5fff98.jpg"
+  },
+  {
+    "revision": "b3832ab9dac2a0e8d0fcbbd22e886c70",
+    "url": "/static/media/cotons2.b3832ab9.jpg"
+  },
+  {
+    "revision": "de7467b3928f33df59bc345a2b89915b",
+    "url": "/static/media/dissertationlogo.de7467b3.jpg"
+  },
+  {
+    "revision": "723792f0573846cfafbd2080da2537e4",
+    "url": "/static/media/graduation.723792f0.jpg"
+  },
+  {
+    "revision": "5895070a1cb5b5f31734f5949acb253e",
+    "url": "/static/media/judo.5895070a.jpg"
+  }
+]);
