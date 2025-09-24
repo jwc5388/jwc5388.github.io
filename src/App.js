07f686e3fa9ae2eb9c2d6f9f5e5537756a2d2a6e
Jae <peter.jaewoochang@gmail.com>
2025-09-24 15:58:16 +0900

    dissertation edit

--- src/App.js
+++ src/App.js
@@ -912,13 +912,13 @@ function App() {
               dates={lang==='ENG' ? "Graduated June 2024" : "2024년 6월 졸업"}
               details={[
                 <div key="dissertation1" className="download-link">
-                  <a href={dissertation1} target="_blank" rel="noopener noreferrer">
+                  <a href={dissertation1} target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
                     <img src={dissImage} alt="View Dissertation 1" className="download-icon" />
                   </a>
                   <span>{lang==='ENG' ? "Computer Science Dissertation" : "컴퓨터과학 학위논문"}</span>
                 </div>,
                 <div key="dissertation2" className="download-link">
-                  <a href={dissertation2} target="_blank" rel="noopener noreferrer">
+                  <a href={dissertation2} target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
                     <img src={dissImage} alt="View Dissertation 2" className="download-icon" />
                   </a>
                   <span>{lang==='ENG' ? "Mathematics Dissertation" : "수학 학위논문"}</span>
