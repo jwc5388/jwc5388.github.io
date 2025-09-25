9b10f5fe0fe23f8f64baf0b8886de5a4ca904a1c
Jae <peter.jaewoochang@gmail.com>
2025-09-25 16:55:14 +0900

    kdigital

--- src/App.js
+++ src/App.js
@@ -1004,7 +1004,7 @@ function App() {
               dates="2025"
               details={getDetails(hackathon_ENG, hackathon_KOR, hackathon_media)}
               isExpanded={expandedSections.engagements.entries.kdigitalHackathon}
-              onClick={() => toggleEntry('engagements', 'quantumAIHackathon')}
+              onClick={() => toggleEntry('engagements', 'kdigitalHackathon')}
             />
 
             <Entry
